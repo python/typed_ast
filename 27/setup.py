d71f35f7af389e8597aba165a9e5176eaa2f71cd
David Fisher <ddfisher@dropbox.com>
2016-02-19 12:35:05 -0800

    remove unused files WIP

---
+++ 27/setup.py
@@ -0,0 +1,27 @@
+from distutils.core import setup, Extension
+
+_typed_ast = Extension(
+    '_typed_ast',
+    include_dirs = ['Include'],
+    sources = [
+        'Parser/acceler.c',
+        'Parser/bitset.c',
+        'Parser/grammar.c',
+        'Parser/grammar1.c',
+        'Parser/node.c',
+        'Parser/parser.c',
+        'Parser/parsetok.c',
+        'Parser/tokenizer.c',
+        'Python/ast.c',
+        'Python/graminit.c',
+        'Python/Python-ast.c',
+        'Custom/typed_ast.c'
+    ])
+
+
+setup (name = 'Typed AST Module',
+       version = '0.1',
+       description = '',
+       package_dir = {'': 'Lib'},
+       py_modules = ['typed_ast'],
+       ext_modules = [_typed_ast])
