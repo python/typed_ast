dbe0b350ec2392ec19c1f4de646a3397a077a8ac
David Fisher <ddfisher@dropbox.com>
2016-05-23 15:30:08 -0700

    Use setuptools when available
    This is necessary for providing wheels.  See #3.

--- setup.py
+++ setup.py
@@ -1,4 +1,7 @@
-from distutils.core import setup, Extension
+try:
+    from setuptools import setup, Extension
+except ImportError:
+    from distutils.core import setup, Extension
 
 _ast27 = Extension(
     '_ast27',
