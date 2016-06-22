0df234e89d47bf4434e877394ad01a7df4d7ac37
David Fisher <ddfisher@dropbox.com>
2016-06-22 16:48:24 -0700

    Bump to version 0.5.3

--- setup.py
+++ setup.py
@@ -78,7 +78,7 @@ comments.  The primary goals of this package are correctness and speed.
 """.strip()
 
 setup (name = 'typed-ast',
-       version = '0.5.2',
+       version = '0.5.3',
        description = 'fork of Python 2 and 3 ast modules with type comment support',
        long_description = long_description,
        author = 'David Fisher',
