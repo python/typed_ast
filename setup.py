f7e804d3640cff721c7d540edd3882378c05af31
David Fisher <ddfisher@dropbox.com>
2017-07-19 13:35:57 -0700

    Add __version__

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import ast
+import re
 import sys
 if sys.version_info[0] < 3 or sys.version_info[1] < 3:
     sys.exit('Error: typed_ast only runs on Python 3.3 and above.')
@@ -86,8 +88,14 @@ Python AST (plus type comments), and are both fast and correct, as they are
 based on the CPython 2.7 and 3.6 parsers.
 """.strip()
 
+_version_re = re.compile(r'__version__\s+=\s+(?P<version>.*)')
+
+with open('typed_ast/__init__.py', 'r', encoding='utf8') as f:
+    version = _version_re.search(f.read()).group('version')
+    version = str(ast.literal_eval(version))
+
 setup (name = 'typed-ast',
-       version = '1.1.0',
+       version = version,
        description = 'a fork of Python 2 and 3 ast modules with type comment support',
        long_description = long_description,
        author = 'David Fisher',
