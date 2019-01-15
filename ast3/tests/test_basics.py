b795010f18cecbff2651833a3482112e036c8b2e
Guido van Rossum <guido@dropbox.com>
2019-01-15 15:03:24 -0800

    Add a very simple test

---
+++ ast3/tests/test_basics.py
@@ -0,0 +1,7 @@
+def test_dunder_ast3():
+    print()
+    import _ast3
+    print(_ast3.__file__)
+    with open(__file__) as f:
+        tree = _ast3._parse(f.read(), __file__, 'exec', 7)
+        print(tree)
