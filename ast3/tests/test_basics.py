0b8423977a3c58e838da5296cd569727d7e7c945
Guido van Rossum <guido@dropbox.com>
2019-01-16 13:36:07 -0800

    Non-working test

--- ast3/tests/test_basics.py
+++ ast3/tests/test_basics.py
@@ -3,5 +3,7 @@ def test_dunder_ast3():
     import _ast3
     print(_ast3.__file__)
     with open(__file__) as f:
-        tree = _ast3._parse(f.read(), __file__, 'exec', 7)
+        tree = _ast3._parse(f.read(), __file__, 'exec', 7)  # type: ignore
         print(tree)
+
+a = 12  # type: int
