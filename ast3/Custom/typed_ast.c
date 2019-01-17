56a52e0b19a3a7ab6953d481f93b367457dc1244
Guido van Rossum <guido@dropbox.com>
2019-01-16 17:41:02 -0800

    Make it C89 compatible, for Python 3.4 on Windows

--- ast3/Custom/typed_ast.c
+++ ast3/Custom/typed_ast.c
@@ -222,12 +222,13 @@ string_object_to_c_ast(const char *s, PyObject *filename, int start,
     PyCompilerFlags localflags;
     perrdetail err;
     int iflags = PARSER_FLAGS(flags);
+    node *n;
 
     if (feature_version >= 7)
         iflags |= PyPARSE_ALWAYS_ASYNC;
-    node *n = Ta3Parser_ParseStringObject(s, filename,
-                                         &_Ta3Parser_Grammar, start, &err,
-                                         &iflags);
+    n = Ta3Parser_ParseStringObject(s, filename,
+                                    &_Ta3Parser_Grammar, start, &err,
+                                    &iflags);
     if (flags == NULL) {
         localflags.cf_flags = 0;
         flags = &localflags;
