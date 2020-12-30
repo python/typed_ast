72d947f40ed07f4684c60ce382d93d3b414103eb
hauntsaninja <>
2020-12-30 09:50:09 +0000

    Remove reference to PyNode_ListTree for Python 3.10
    Fixes #150

--- ast3/Parser/parser.c
+++ ast3/Parser/parser.c
@@ -382,22 +382,6 @@ showtree(grammar *g, node *n)
         printf("? ");
 }
 
-void
-printtree(parser_state *ps)
-{
-    if (Py_DebugFlag) {
-        printf("Parse tree:\n");
-        dumptree(ps->p_grammar, ps->p_tree);
-        printf("\n");
-        printf("Tokens:\n");
-        showtree(ps->p_grammar, ps->p_tree);
-        printf("\n");
-    }
-    printf("Listing:\n");
-    PyNode_ListTree(ps->p_tree);
-    printf("\n");
-}
-
 #endif /* Py_DEBUG */
 
 /*
