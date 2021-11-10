ee9e3c324a01761b55347f45c4858146ccf5fd67
Shantanu <12621235+hauntsaninja@users.noreply.github.com>
2021-11-10 11:21:43 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -91,7 +91,7 @@ Python AST (plus type comments), and are both fast and correct, as they are
 based on the CPython 2.7 and 3.7 parsers.
 
 **Note:** The `ast` module of Python 3.8+ supports all features of `typed_ast`.
-On the other hand, `typed_ast` does not support Python 3.8 or higher code.
+On the other hand, `typed_ast` does not support parsing Python 3.8 or higher code.
 We recommend to use `ast` on Python 3.8 or above.
 """.strip()
 
